a50cc762584d97920fe1e05857411b4f704e45cb
ssvamsee <siddamvamsee@gmail.com>
2025-12-29 17:10:03 +0530

    removed vite federation

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    federation({
-      name: 'genAiDocument',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './App': './src/App.jsx',
-      },
-      shared: {
-        react: {
-          singleton: true,
-          requiredVersion: '^19.2.0',
-        },
-        'react-dom': {
-          singleton: true,
-          requiredVersion: '^19.2.0',
-        }
-      }
-    }),
     react(),
     tailwindcss(),
   ],
-  build: {
-    modulePreload: false,
-    target: 'esnext',
-    minify: false,
-    cssCodeSplit: false
-  },
   server: {
     port: 5001,
-    strictPort: true,
-    cors: true,
   },
   preview: {
     port: 5001,
-    strictPort: true,
-    cors: true,
   }
 })
